775eae36276a5774fe126d1f79eb391fb29523f7
nn <>
2023-09-25 22:14:43 +0200

    Additions of finished week1 Part1 and Part2

---
+++ Week1/ex1.js
@@ -0,0 +1,16 @@
+
+
+window.onload = function init() {
+    canvas = document.getElementById("c");
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn’t available");
+    }
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
+    render();
+}
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+}
